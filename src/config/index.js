da347d052b5228f8c67762bc3e1af407ae554b4b
gabrielcoutinhose <gabrielctnhpro@gmail.com>
2025-06-28 09:45:09 -0300

    Finalizing the structure for API consumption

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+const config = {
+  api: {
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    key: import.meta.env.VITE_API_KEY,
+  },
+};
+
+export default config;
---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import config from '@/config';
+
+const api = axios.create({
+  baseURL: config.api.baseURL,
+});
+
+export const fetchPopularMovies = async () => {
+  const { data } = await api.get('/movie/popular', {
+    params: {
+      api_key: config.api.key,
+      language: 'pt-BR',
+    },
+  });
+  return data.results;
+};
